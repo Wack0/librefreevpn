cc4639f077b8e15e0abe60d2253dcaf084fd548f
Rairii <2650838+Wack0@users.noreply.github.com>
2026-02-04 22:21:39 +0000

    freevpnpc: allow resize of config list dialog

--- FreeVPNPC/ConfigListDialog.Designer.cs
+++ FreeVPNPC/ConfigListDialog.Designer.cs
@@ -31,13 +31,19 @@
             this.buttonSave = new System.Windows.Forms.Button();
             this.listBoxServers = new System.Windows.Forms.ListBox();
             this.textBoxConfig = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(406, 480);
+            this.buttonSave.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonSave.Location = new System.Drawing.Point(0, 492);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.Size = new System.Drawing.Size(302, 23);
             this.buttonSave.TabIndex = 0;
             this.buttonSave.Text = "&Save...";
             this.buttonSave.UseVisualStyleBackColor = true;
@@ -45,35 +51,61 @@
             // 
             // listBoxServers
             // 
+            this.listBoxServers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.listBoxServers.FormattingEnabled = true;
-            this.listBoxServers.Location = new System.Drawing.Point(13, 13);
+            this.listBoxServers.Location = new System.Drawing.Point(3, 3);
             this.listBoxServers.Name = "listBoxServers";
-            this.listBoxServers.Size = new System.Drawing.Size(277, 459);
+            this.listBoxServers.Size = new System.Drawing.Size(279, 472);
             this.listBoxServers.TabIndex = 1;
             this.listBoxServers.SelectedIndexChanged += new System.EventHandler(this.listBoxServers_SelectedIndexChanged);
             // 
             // textBoxConfig
             // 
+            this.textBoxConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxConfig.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxConfig.Location = new System.Drawing.Point(297, 13);
+            this.textBoxConfig.Location = new System.Drawing.Point(3, 3);
             this.textBoxConfig.Multiline = true;
             this.textBoxConfig.Name = "textBoxConfig";
-            this.textBoxConfig.Size = new System.Drawing.Size(282, 461);
+            this.textBoxConfig.Size = new System.Drawing.Size(295, 480);
             this.textBoxConfig.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBoxServers);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxConfig);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonSave);
+            this.splitContainer1.Size = new System.Drawing.Size(591, 515);
+            this.splitContainer1.SplitterDistance = 285;
+            this.splitContainer1.TabIndex = 3;
             // 
             // ConfigListDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(591, 515);
-            this.Controls.Add(this.textBoxConfig);
-            this.Controls.Add(this.listBoxServers);
-            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "ConfigListDialog";
             this.Text = "FreeVPNPC - VPN Configs";
             this.Load += new System.EventHandler(this.ConfigListDialog_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -82,5 +114,6 @@
         private System.Windows.Forms.Button buttonSave;
         private System.Windows.Forms.ListBox listBoxServers;
         private System.Windows.Forms.TextBox textBoxConfig;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
